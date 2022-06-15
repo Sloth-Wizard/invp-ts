2ea35e29a72eefbf08e94ec7380274aa54cc3c31
Sloth-Wizard <alexis.abeloos@gmail.com>
2022-06-15 10:40:26 +0200

    made the module async and fix wrong use of splice and indexOf methods

--- invp.ts
+++ invp.ts
@@ -19,8 +19,13 @@ interface Threshold {
 }
 
 interface Options {
-    offset: Intersection,
-    threshold: number
+    offset: Intersection;
+    threshold: number;
+}
+
+interface Reaveal {
+    element: HTMLElement;
+    reveal: boolean;
 }
 
 /**
@@ -28,9 +33,10 @@ interface Options {
  * Inpired by in-view.js
  */
 export class InVp {
-    is_visible_class: string = 'is-visible';
-    debounce_time: number = 200;
-    options: Options = {
+    protected visible_elements: number = 0;
+    protected is_visible_class: string = 'is-visible';
+    protected debounce_time: number = 200;
+    protected options: Options = {
         offset: {
             top: 0,
             right: 0,
@@ -48,11 +54,11 @@ export class InVp {
     /**
      * Get the elements we need to link to an observable
      * 
-     * @param query
+     * @param query - string
      * 
-     * @returns NodeListOf<HTMLElement>
+     * @returns `NodeListOf<T>`
      */
-    get_elements(query: string): NodeListOf<HTMLElement> {
+    private get_elements<T extends HTMLElement>(query: string): NodeListOf<T> {
         return document.querySelectorAll(query);
     }
 
@@ -60,60 +66,65 @@ export class InVp {
      * Observe the elements comming in the viewport
      * 
      * @param elements
+     * 
+     * @returns `Promise<void>`
      */
-    observe(elements: NodeListOf<HTMLElement>): void {
+    private async observe(elements: NodeListOf<HTMLElement>): Promise<void> {
         // Copy the NodeList after registering all already visible elements into an array so we can slice it later
-        let nodes: Array<HTMLElement> = this.register([].slice.call(elements, 0));
+        return await this.register([].slice.call(elements, 0)).then(async els => {
+            if (!els) return; // End silently because we just received the unsub signal
+            // Events to subscribe
+            const SCROLLING: Observable<Event>  = fromEvent(document, 'wheel').pipe(debounce(() => interval(this.debounce_time)));
+            const RESIZE: Observable<Event>     = fromEvent(window, 'resize').pipe(debounce(() => interval(this.debounce_time)));
 
-        // Events to subscribe
-        const SCROLLING: Observable<Event>  = fromEvent(document, 'wheel').pipe(debounce(() => interval(this.debounce_time)));
-        const RESIZE: Observable<Event>     = fromEvent(window, 'resize').pipe(debounce(() => interval(this.debounce_time)));
-        
-        // Merge all those events
-        const EVENTS: Observable<Event> = merge(
-            SCROLLING,
-            RESIZE
-        );
+            // Merge all those events
+            const EVENTS: Observable<Event> = merge(
+                SCROLLING,
+                RESIZE
+            );
 
-        // Subscribe to all events
-        const SUBSCRIPTION: Subscription = EVENTS.subscribe(() => {
-            this.register(nodes, SUBSCRIPTION);
+            // Subscribe to all events
+            const SUBSCRIPTION: Subscription = EVENTS.subscribe(async () => {
+                await this.register(els, SUBSCRIPTION);
+            });
         });
     }
 
     /**
      * Register the elements in viewport
      * 
-     * @param nodes 
-     * @param subscription
+     * @param nodes - HTMLElement[]
+     * @param subscription - Subscription
      * 
-     * @returns Array<HTMLElement>
+     * @returns `Promise<HTMLElement[] | void>`
      */
-    register(nodes: Array<HTMLElement>, subscription?: Subscription): Array<HTMLElement> {
-        nodes.forEach((el, i) => {
-            if (this.in_viewport(el, {offset: this.options.offset, threshold: this.options.threshold})) {
-                el.classList.add(this.is_visible_class);
-
-                // Slice the index node because it has been loaded
-                nodes.splice(i, 1);
-
-                // When the nodes are empty, it means everything has been loaded so stop the subscription
-                if (subscription && nodes.length === 0) subscription.unsubscribe();
-            }
-        });
+    private async register(nodes: HTMLElement[], subscription?: Subscription): Promise<HTMLElement[] | void> {
+        if (subscription && this.visible_elements === nodes.length)
+            return subscription.unsubscribe();
+        
+        for (const NODE of nodes) {
+            await this.in_viewport(NODE, this.options).then(async r => {
+                if (r.reveal)
+                    await this.addclass(r.element).then(b => {
+                        if (b)
+                            this.visible_elements = this.visible_elements+1;
+                    });
+                    
+            });
+        }
 
         return nodes;
     }
 
     /**
-     * Check whether an element is in the viewport or not
+     * ## Check whether an element is in the viewport or not
      * 
-     * @param el 
-     * @param options
+     * @param el - HTMLElement
+     * @param options - Options
      * 
-     * @returns boolean
+     * @returns `Promise<Reaveal>`
      */
-    in_viewport(el: HTMLElement, options: {offset: Intersection, threshold: number}): boolean {
+    private async in_viewport(el: HTMLElement, options: Options): Promise<Reaveal> {
         const BCR: ViewportPosition = el.getBoundingClientRect();
         const INTERSECTION: Intersection = {
             top: BCR.bottom,
@@ -127,9 +138,28 @@ export class InVp {
             y: options.threshold * BCR.height
         }
 
-        return INTERSECTION.top     > (options.offset.top       + THRESHOLD.y)
+        return {
+            element: el,
+            reveal: INTERSECTION.top     > (options.offset.top       + THRESHOLD.y)
             && INTERSECTION.right   > (options.offset.right     + THRESHOLD.x)
             && INTERSECTION.bottom  > (options.offset.bottom    + THRESHOLD.y)
             && INTERSECTION.left    > (options.offset.left      + THRESHOLD.x)
+        }
+    }
+
+    /**
+     * ## Add a class to the element and checks if it was added
+     * 
+     * @param el - HTMLElement
+     * 
+     * @returns `Promise<boolean>`
+     */
+    private async addclass(el: HTMLElement): Promise<boolean> {
+        // Class already there when going throught again so send a false signal
+        if (el.classList.contains(this.is_visible_class))
+            return false;
+
+        el.classList.add(this.is_visible_class);
+        return el.classList.contains(this.is_visible_class);
     }
 }
